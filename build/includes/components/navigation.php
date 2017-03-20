c4add04b1c92b307947dbf31202e1b2a807ae5fd
Patrick Sunico <apatricksunico@gmail.com>
2017-03-20 23:40:26 +0800

    Update Flexbox Dropdown

--- build/includes/components/navigation.php
+++ build/includes/components/navigation.php
@@ -1,21 +1,69 @@
-  <nav class="flex-nav">
-    <span class="logo-placeholder">
-      <a href="">Logo Here</a>
-    </span>
-    <div class="toggleNav">
-      ☰
+  <!--<nav>
+    <div class="flex-nav-container">
+      <span class="logo-placeholder">
+        <a href="#">
+          <?php include('includes/components/svg_logo.php'); ?>
+        </a>
+      </span>
+      <div class="toggleNav">
+        ☰
+      </div>
+      <ul class="ul-container">
+        <li>
+          <a href="#">Platforms <i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+          <ul class="sub-menu">
+            <li>
+              <a href="#">
+                Sub Menu
+              </a>  
+            </li>  
+          </ul>  
+        </li>
+        <li><a href="#">Games by Genre <i class="fa fa-chevron-down" aria-hidden="true"></i></a></li>
+        <li><a href="#">Upcoming <i class="fa fa-chevron-down" aria-hidden="true"></i></a></li>
+        <li><a href="#">Browse by Ratings <i class="fa fa-chevron-down" aria-hidden="true"></i></a></li>
+        <li><a href="#">Cart <i class="fa fa-chevron-down" aria-hidden="true"></i></a></li>
+        <li><a href="#">Account <i class="fa fa-chevron-down" aria-hidden="true"></i></a></li>
+      </ul>
     </div>
-    <ul class="ul-container">
-      <li><a href="#">Item 01</a></li>
-      <li><a href="#">Item 02</a></li>
-      <li><a href="#">Item 03</a></li>
-      <li><a href="#">Item 04</a></li>
-      <li><a href="#">Item 05</a></li>
-      <li><a href="#">Item 06</a></li>
+  </nav>-->
 
-      <li class="social"><a class="fa fa-twitter" href="#"></a></li>
-      <li class="social"><a class="fa fa-facebook" href="#"></a></li>
-      <li class="social"><a class="fa fa-github" href="#"></a></li>
-      <li class="social"><a class="fa fa-instagram" href="#"></a></li>
-    </ul>
-  </nav>
+
+<nav>
+  <ul>
+    <li class="has-children">
+      <a href="#">Home</a>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">About</a>
+    </li>
+        <li>
+      <a href="#">About</a>
+    </li>
+    <li class="has-children">
+      <a href="#">Products</a>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</nav>
--- build/index.php
+++ build/index.php
@@ -2,8 +2,7 @@
   include('includes/global/head.php');
 ?>
   <?php include('includes/components/navigation.php');?>
-  <a href="/portfolio.php"> Portfolio Site </a>
-  <a href="/news.php"> News Site</a>
+  
 <?php
   include('includes/global/footer.php');
 ?>
