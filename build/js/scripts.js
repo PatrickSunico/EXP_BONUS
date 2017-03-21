e4a5b6314ab4febd0e53faa1a3752c808c8d17d4
Patrick Sunico <apatricksunico@gmail.com>
2017-03-21 21:53:31 +0800

    Refactor

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -19,17 +19,26 @@ $(window).on('resize', function() {
 });
 
 $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
     dots: true,
-    centerMode: true,
-    focusOnSelect: true
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
 });
+
+
+// $('.slider-for').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     asNavFor: '.slider-nav'
+// });
+// $('.slider-nav').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     asNavFor: '.slider-for',
+//     dots: true,
+//     centerMode: true,
+//     focusOnSelect: true
+// });
