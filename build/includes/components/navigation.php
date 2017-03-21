2ce72923de0ed297d2527b9a41ae2e76331c4b81
Patrick Sunico <apatricksunico@gmail.com>
2017-03-21 14:41:21 +0800

    Update

--- build/includes/components/navigation.php
+++ build/includes/components/navigation.php
@@ -1,69 +1,107 @@
-  <!--<nav>
-    <div class="flex-nav-container">
-      <span class="logo-placeholder">
-        <a href="#">
-          <?php include('includes/components/svg_logo.php'); ?>
-        </a>
-      </span>
-      <div class="toggleNav">
-        ☰
-      </div>
-      <ul class="ul-container">
-        <li>
-          <a href="#">Platforms <i class="fa fa-chevron-down" aria-hidden="true"></i></a>
-          <ul class="sub-menu">
-            <li>
-              <a href="#">
-                Sub Menu
-              </a>  
-            </li>  
-          </ul>  
-        </li>
-        <li><a href="#">Games by Genre <i class="fa fa-chevron-down" aria-hidden="true"></i></a></li>
-        <li><a href="#">Upcoming <i class="fa fa-chevron-down" aria-hidden="true"></i></a></li>
-        <li><a href="#">Browse by Ratings <i class="fa fa-chevron-down" aria-hidden="true"></i></a></li>
-        <li><a href="#">Cart <i class="fa fa-chevron-down" aria-hidden="true"></i></a></li>
-        <li><a href="#">Account <i class="fa fa-chevron-down" aria-hidden="true"></i></a></li>
-      </ul>
-    </div>
-  </nav>-->
-
-
 <nav>
-  <ul>
-    <li class="has-children">
-      <a href="#">Home</a>
-      <ul>
-        <li>
-          <a href="#">Home</a>
+  <div class="flex-nav-container">
+    <span class="logo-placeholder">
+      <a href="#">
+        <?php include('includes/components/svg_logo.php');?>
+      </a>
+    </span>
+    <div class="toggleNav">
+      ☰
+    </div>
+    <ul class="ul-container">
+      <li class="has-children">
+        <a class="primary-menu" href="#">Platforms
+          <i class="fa fa-chevron-down" aria-hidden="true"></i>
+        </a>
+        <ul>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 01</a>
+          </li>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 02</a>
+          </li>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 03</a>
+          </li>
+        </ul>
+      </li>
+      <li class="has-children">
+        <a class="primary-menu" href="#">Games by Genre
+          <i class="fa fa-chevron-down" aria-hidden="true"></i>
+        </a>
+        <ul>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 01</a>
+          </li>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 02</a>
+          </li>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 03</a>
+          </li>
+        </ul>
+      </li>
+      <li class="has-children">
+        <a class="primary-menu" href="#">Upcoming
+          <i class="fa fa-chevron-down" aria-hidden="true"></i>  
+        </a>
+        <ul>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 01</a>
+          </li>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 02</a>
+          </li>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 03</a>
+          </li>
+        </ul>
+      </li>
+      <li class="has-children">
+        <a class="primary-menu" href="#">Browse by Ratings
+            <i class="fa fa-chevron-down" aria-hidden="true"></i>
+        </a>
+
+        <ul>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 01</a>
+          </li>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 02</a>
+          </li>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 03</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a class="primary-menu" href="#">
+          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+          Cart
+        </a>
         </li>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Home</a>
-        </li>
-      </ul>
-    </li>
-    <li>
-      <a href="#">About</a>
-    </li>
-        <li>
-      <a href="#">About</a>
-    </li>
-    <li class="has-children">
-      <a href="#">Products</a>
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Home</a>
-        </li>
-      </ul>
-    </li>
-  </ul>
+      <li class="has-children">
+        <a class="primary-menu" href="#">
+          <i class="icon-account" aria-hidden="true"></i>
+            Account
+          <i class="fa fa-chevron-down" aria-hidden="true"></i>
+        </a>
+        <ul>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 01</a>
+          </li>
+          <li>
+            <a class="sub-menu" href="#">Sub-item 01</a>
+          </li>
+        </ul>
+      </li>
+
+      <!--<li class="social"><a class="fa fa-twitter" href="#"></a></li>
+      <li class="social"><a class="fa fa-facebook" href="#"></a></li>
+      <li class="social"><a class="fa fa-github" href="#"></a></li>
+      <li class="social"><a class="fa fa-instagram" href="#"></a></li>-->
+    </ul>
+  </div>
 </nav>
+
+
