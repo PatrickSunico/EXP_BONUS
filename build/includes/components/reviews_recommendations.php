2ea90e7ff4925943895d3b351044c96da1605802
Patrick Sunico <apatricksunico@gmail.com>
2017-03-22 20:55:09 +0800

    Update

---
+++ build/includes/components/reviews_recommendations.php
@@ -0,0 +1,18 @@
+  <h1 class="h1">Top Reviews & Recommendations</h1>
+  <div class="review_grid">
+      <div class="feature-lg">
+        <img src="../images/feature-1.jpg" alt="Gears of War 4">
+      </div>
+
+      <div class="feature-sm">
+        <div class="feature-sm-a">
+
+          <!-- <div class="feature-media"> -->
+            <img src="../images/feature-2.jpg" alt="Horizon Zero Dawn">
+          <!-- </div> -->
+        </div>
+        <div class="feature-sm-b">
+          <img src="../images/feature-3.jpg" alt="Dishonored 2">
+        </div>
+      </div>
+  </div>
--- build/index.php
+++ build/index.php
@@ -4,9 +4,12 @@
   <?php include('includes/components/navigation.php');?>
   <?php include('includes/components/search.php');?>
   <?php include('includes/components/main_carousel.php'); ?>
-  <?php include('includes/components/carousel_experiment.php');?>
-  
+
+  <div class="feature-wrapper">
+    <?php include('includes/components/reviews_recommendations.php'); ?>
+  </div>
+
+
 <?php
   include('includes/global/footer.php');
 ?>
-
