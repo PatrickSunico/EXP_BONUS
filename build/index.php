c05e6c12de06edb4f6bd72d143a1c64aef9331ab
PatrickSunico <apatricksunico@gmail.com>
2017-03-24 10:14:31 +0800

    Refactor

--- build/index.php
+++ build/index.php
@@ -2,14 +2,14 @@
   include('includes/global/head.php');
 ?>
   <?php include('includes/components/navigation.php');?>
-  <?php include('includes/components/search.php');?>
+  <?php //include('includes/components/search.php');?>
   <?php include('includes/components/main_carousel.php'); ?>
 
-  <div id="feature-wrapper">
-    <?php include('includes/components/reviews_recommendations.php'); ?>
-    <?php include('includes/components/review_carousel.php'); ?>
-    <?php include('includes/components/upcoming_releases.php');?>
-  </div>
+  <!--<div id="feature-wrapper">-->
+    <?php //include('includes/components/reviews_recommendations.php'); ?>
+    <?php //include('includes/components/review_carousel.php'); ?>
+    <?php //include('includes/components/upcoming_releases.php');?>
+  <!--</div>-->
 
 
 <?php
