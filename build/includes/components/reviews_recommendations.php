8f6aaebc10d8904496dc5bad870f8a5d6d74bd8e
PatrickSunico <apatricksunico@gmail.com>
2017-03-26 22:40:35 +0800

    Update Price Overlay

--- build/includes/components/reviews_recommendations.php
+++ build/includes/components/reviews_recommendations.php
@@ -7,15 +7,27 @@
   <div class="masonry-image">
       <div class="item-grp">
         <a href="#" class="img-item-lg">
-          <img src="../images/feature-1.jpg" alt="image">
+          <!-- price overlay-->
+          <div class="price-container"> <!--flexbox container position:relative-->
+            <div class="non-fb-container"> <!-- non flexbox container position:absolute-->
+              <div class="discount"> 
+                <h4> -15% </h4>
+              </div>
+              <div class="price-list">
+                <h5>Price: $ 42.00 <strike>$ 50.00</strike></h5>
+              </div>
+            </div>
+          </div>
+          <!-- end price overlay-->
+          <img src="../images/featured_images/feature-1.jpg" alt="image">
         </a>
       </div>
       <div class="item-grp">
         <a href="#" class="img-item-sm">
-          <img src="../images/feature-2.jpg" alt="image">
+          <img src="../images/featured_images/feature-2.jpg" alt="image">
         </a>
         <a href="#" class="img-item-sm">
-          <img src="../images/feature-3.jpg" alt="image">
+          <img src="../images/featured_images/feature-3.jpg" alt="image">
         </a>
       </div>
     </div>
