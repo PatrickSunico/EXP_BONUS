85a62e767985eaf509da0c7a25966c5423406440
PatrickSunico <apatricksunico@gmail.com>
2017-03-29 16:28:57 +0800

    Initial dist subtree commit

--- build/includes/components/features_carousel.php
+++
@@ -1,8 +0,0 @@
-<div class="featurette_wrapper">
-    <div class="swiper-container feature_carousel">
-        <div class="swiper-wrapper">
-            <?php carousel_images();?>
-        </div>
-        <div class="swiper-pagination"></div>
-    </div>
-</div>
--- build/includes/components/search.php
+++
@@ -1,7 +0,0 @@
-<div class="form-wrapper">
-  <form action="" method="post">
-      <input class="search_textbox" type="text" name="search" placeholder="Search Marketplace..." autocomplete="off">
-  </form>
-</div>
-
-
