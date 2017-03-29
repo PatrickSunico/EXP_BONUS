a6e2da4f43a4736494310baf9610fecd92e9a8b2
PatrickSunico <apatricksunico@gmail.com>
2017-03-29 10:03:25 +0800

    Browser Refactoring 3

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task('sass', function() {
     return gulp.src(rawPaths.scss)
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(rename('main.min.css'))
-        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(autoprefixer(autoprefixerOptions.browsers))
         .pipe(gulp.dest(out.cssOut));
 });
 
